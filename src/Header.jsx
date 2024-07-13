40b1c52f6bf5e827075306a7c29b30b03273c5a7
JohnDelMando <jhnpnglnn@gmail.com>
2024-07-12 19:53:02 -0600

    added banana image

--- src/Header.jsx
+++ src/Header.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import './Header.css';
 
 function Header() {
@@ -7,7 +8,9 @@ function Header() {
       <div className="logo">Logo</div>
       <input type="text" className="search" placeholder="Search for Products" />
       <div className="profile-cart">
-        <div className="profile">Profile</div>
+        <div className="profile">
+          <Link to='/profile'>Profile</Link>
+        </div>
         <div className="cart">Cart</div>
       </div>
     </header>
--- src/app.jsx
+++ src/app.jsx
@@ -1,28 +1,46 @@
 import React from 'react';
+import { BrowserRouter as Router, Routes, Route, useLocation } from 'react-router-dom';
 import './App.css';
 import Header from './Header';
 import ItemList from './Itemlist';
-
+import Profile from './Profile';
 
 function App() {
   return (
-    <div className="App">
-      <Header />
-          <div className="categories">
-            <button>Produce</button>
-            <button>Baby</button>
-            <button>Home Esentials</button>
-            <button>Meat</button>
-            <button>Clothing</button>
-            <button>Health</button>
-            <button>Beauty</button>
-            <button>Category</button>
-            <button>Checkout</button>
-            <button>Cart</button>
-          </div>
-  
-      <ItemList />
-    </div>
+    <Router>
+      <div className="App">
+        <Header />
+        <MainContent />
+      </div>
+    </Router>
+  );
+}
+
+function MainContent() {
+  const location = useLocation();
+  const showContent = location.pathname !== '/profile';
+
+  return (
+    <>
+      {showContent && (
+        <div className="categories">
+          <button>Produce</button>
+          <button>Baby</button>
+          <button>Home Essentials</button>
+          <button>Meat</button>
+          <button>Clothing</button>
+          <button>Health</button>
+          <button>Beauty</button>
+          <button>Category</button>
+          <button>Checkout</button>
+          <button>Cart</button>
+        </div>
+      )}
+      <Routes>
+        <Route path="/profile" element={<Profile />} />
+      </Routes>
+      {showContent && <ItemList />}
+    </>
   );
 }
 
