3cb00f755944c5c6e363d2b39cad220d0219018d
EJG2023 <133731036+EJG2023@users.noreply.github.com>
2024-07-14 16:40:32 -0600

    Fixed spaces for Cart.jsx file

--- src/Cart.jsx
+++ src/Cart.jsx
@@ -1,11 +1,8 @@
 import { useState } from 'react'
-// import { Routes, Route, useNavigate } from 'react-router-dom'
-// import Checkout from './Checkout'
 import { useNavigate } from 'react-router-dom';
 import './Cart.css'
 import chicken from './assets/chicken.jpg'
 import steak from './assets/Steak.jpg'
-
 
 
 function Cart() {
@@ -26,46 +23,39 @@ function Cart() {
     navigate('/checkout');
   };
 
-  // const navigate1 = useNavigate();
-  // const goToHome = () => {
-  //   navigate('/app');
-  // };
-
-  // <button type="button" className="cart-home-button" onClick={goToHome}>Home</button>
 
   return (
 
-    
-      <body className="cart-body">
-        <div className="cart-page">
+    <body className="cart-body">
+      <div className="cart-page">
 
         <div className="cart-container">
-          
+
           <h1>YOUR CART</h1>
 
           <div className="cart-item-container">
 
-          <div className="cart-item">
+            <div className="cart-item">
               <div className="cart-item-picture">
                 <img src={chicken} alt="chicken" id="cart-item-image" />
               </div>
               <div className="cart-item-description">
-      
+
                 <p>Description: Whole Chicken</p>
 
-                  <div className="cart-quantity-container">
+                <div className="cart-quantity-container">
 
-                    <div className="cart-quantity-text">Quantity:  </div>
+                  <div className="cart-quantity-text">Quantity:  </div>
 
-                    <div className="cart-quantity">
+                  <div className="cart-quantity">
                     <button className="quantity-btn-minus" onClick={() => decreaseQuantity('item1')}>-</button>
                     <input type="number" className="quantity-input" value={quantities.item1} readOnly />
                     <button className="quantity-btn-plus" onClick={() => increaseQuantity('item1')}>+</button>
-                    </div>
-
                   </div>
-                 
-                  <a href="https://www.w3schools.com/" className="cart-remove-link">Remove</a> 
+
+                </div>
+
+                <a href="https://www.w3schools.com/" className="cart-remove-link">Remove</a>
 
                 <p>Price: $6.99</p>
               </div>
@@ -82,17 +72,17 @@ function Cart() {
 
                 <div className="cart-quantity-container">
 
-                    <div className="cart-quantity-text">Quantity:  </div>
+                  <div className="cart-quantity-text">Quantity:  </div>
 
-                    <div className="cart-quantity">
+                  <div className="cart-quantity">
                     <button className="quantity-btn-minus" onClick={() => decreaseQuantity('item2')}>-</button>
                     <input type="number" className="quantity-input" value={quantities.item2} readOnly />
                     <button className="quantity-btn-plus" onClick={() => increaseQuantity('item2')}>+</button>
-                    </div>
-
                   </div>
 
-                  <a href="https://www.w3schools.com/" className="cart-remove-link">Remove</a> 
+                </div>
+
+                <a href="https://www.w3schools.com/" className="cart-remove-link">Remove</a>
 
                 <p>Price: $9.99</p>
               </div>
@@ -103,22 +93,22 @@ function Cart() {
                 <img src={chicken} alt="chicken" id="cart-item-image" />
               </div>
               <div className="cart-item-description">
-                
+
                 <p>Description: Whole Chicken</p>
 
                 <div className="cart-quantity-container">
 
-                    <div className="cart-quantity-text">Quantity:  </div>
+                  <div className="cart-quantity-text">Quantity:  </div>
 
-                    <div className="cart-quantity">
+                  <div className="cart-quantity">
                     <button className="quantity-btn-minus" onClick={() => decreaseQuantity('item3')}>-</button>
                     <input type="number" className="quantity-input" value={quantities.item3} readOnly />
                     <button className="quantity-btn-plus" onClick={() => increaseQuantity('item3')}>+</button>
-                    </div>
-
                   </div>
 
-                  <a href="https://www.w3schools.com/" className="cart-remove-link">Remove</a> 
+                </div>
+
+                <a href="https://www.w3schools.com/" className="cart-remove-link">Remove</a>
 
                 <p>Price: $6.99</p>
               </div>
@@ -129,33 +119,31 @@ function Cart() {
                 <img src={steak} alt="steak" id="cart-item-image" />
               </div>
               <div className="cart-item-description">
-                
+
                 <p>Description: Steak</p>
 
                 <div className="cart-quantity-container">
 
-                    <div className="cart-quantity-text">Quantity:  </div>
+                  <div className="cart-quantity-text">Quantity:  </div>
 
-                    <div className="cart-quantity">
+                  <div className="cart-quantity">
                     <button className="quantity-btn-minus" onClick={() => decreaseQuantity('item4')}>-</button>
                     <input type="number" className="quantity-input" value={quantities.item4} readOnly />
                     <button className="quantity-btn-plus" onClick={() => increaseQuantity('item4')}>+</button>
-                    </div>
-
                   </div>
 
-                  <a href="https://www.w3schools.com/" className="cart-remove-link">Remove</a> 
+                </div>
 
+                <a href="https://www.w3schools.com/" className="cart-remove-link">Remove</a>
 
                 <p>Price: $9.99</p>
               </div>
             </div>
 
             <p>Your Total is: $100</p>
-            
+
           </div>
 
-          
           <button type="button" className="cart-button-checkout" onClick={goToCheckout}>Checkout</button>
           <button type="button" className="cart-button-meal">Meal Planning</button>
         </div>
@@ -163,21 +151,9 @@ function Cart() {
         <footer>&copy; Green Basket 2024</footer>
       </div>
 
-
-      </body>
-
-
+    </body>
   )
 }
 
-
-// function App() {
-//   return (
-//     <Routes>
-//       <Route path="/" element={<Home />} />
-//       <Route path="/checkout" element={<Checkout />} />
-//     </Routes>
-//   )
-// }
 
 export default Cart;
