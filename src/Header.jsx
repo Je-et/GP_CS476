249a80895fc211832998e8c3153d261a6c9da94f
JohnDelMando <jhnpnglnn@gmail.com>
2024-07-14 17:43:32 -0600

    cleaned the texts

--- src/Header.jsx
+++ src/Header.jsx
@@ -5,14 +5,16 @@ import './Header.css';
 function Header() {
   return (
     <header className="header">
-      <div className="logo">Logo</div>
+      <div className="logo">
+        <Link to='/' style={{ textDecoration: 'none' }}>Logo</Link>
+      </div>
       <input type="text" className="search" placeholder="Search for Products" />
       <div className="profile-cart">
         <div className="profile">
-          <Link to='/profile'>Profile</Link>
+          <Link to='/profile' style={{ textDecoration: 'none' }}>Profile</Link>
         </div>
         <div className="cart">
-        <Link to='/cart'>Cart</Link>
+          <Link to='/cart' style={{ textDecoration: 'none' }}>Cart</Link>
         </div>
       </div>
     </header>
