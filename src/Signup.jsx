de9fa1624ac4d32b7b1b45c41f7f13e4f301cb48
Karlokohan <demonictrioxide@gmail.com>
2024-07-14 20:49:10 -0600

    Create Signup.jsx

---
+++ src/Signup.jsx
@@ -0,0 +1,34 @@
+import React from 'react';
+import './Signup.css';
+import Loginheader from './Loginheader';
+
+function Signup() {
+  return (
+    <div>
+    <Loginheader/>
+      <div className="background-middle-signin">
+        <div className="signup-container">
+          <div className="signup-form">
+            <header className='signup-header'>Create an Account</header>
+            <form action="#" method="POST">
+              <input type="email" name="email" placeholder="Email Address" className='signup-input' />
+              <input type="text" name="username" placeholder="Username" className='signup-input' />
+              <input type="password" name="password" placeholder="Password" className='signup-input' />
+              <input type="password" name="confirmPassword" placeholder="Confirm Password" className='signup-input' />
+              <button className="signup-button">GET STARTED</button>
+
+              <p className='signup-p'>Already have an account? Click<a href="Login.jsx" id="register"> here</a>!</p>
+
+            </form>
+          </div>
+        </div>
+      </div>
+
+      <footer>
+        <p>© Green Basket 2024</p>
+      </footer>
+    </div>  
+  );
+};
+
+export default Signup;
