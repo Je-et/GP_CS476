d12bdbe22ee70d60f373df581c699cf7c097c82a
Karlokohan <demonictrioxide@gmail.com>
2024-07-14 20:48:19 -0600

    Create Loginheader.jsx

---
+++ src/Loginheader.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './Loginheader.css';
+
+function Loginheader() {
+  return (
+    <header className="loginheader">
+      <div className="logo">Logo</div>
+
+    </header>
+  );
+}
+
+export default Loginheader;
