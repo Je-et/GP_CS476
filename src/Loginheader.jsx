1edad4955c64d73e698364141c8c67fdbc173a00
Karlokohan <demonictrioxide@gmail.com>
2024-07-14 22:52:00 -0600

    Changed Loginheader.jsx

--- src/Loginheader.jsx
+++ src/Loginheader.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import './Loginheader.css';
 
 function Loginheader() {
   return (
     <header className="loginheader">
-      <div className="logo">Logo</div>
+      <div className="logo"><Link to='/' style={{ textDecoration: 'none' }}>Logo</Link></div>
 
     </header>
   );
