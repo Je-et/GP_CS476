2eb6e53c0dde31fb1bab336b04d785dc9e29a7b2
EJG2023 <133731036+EJG2023@users.noreply.github.com>
2024-07-17 15:13:35 -0600

    Updated Loginheader.jsx file

--- src/Loginheader.jsx
+++ src/Loginheader.jsx
@@ -1,11 +1,16 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import './Loginheader.css';
+import Logo from './assets/Logo.png';
 
 function Loginheader() {
   return (
     <header className="loginheader">
-      <div className="logo"><Link to='/' style={{ textDecoration: 'none' }}>Logo</Link></div>
+      <div className="logo">
+        <Link to='/'>
+          <img src={Logo} alt="Logo" className="header-logo-image" />
+        </Link>
+      </div>
 
     </header>
   );
