993ed0d7b2199643e7e6447d1fbee8bb5d73eda5
EJG2023 <133731036+EJG2023@users.noreply.github.com>
2024-07-17 15:13:02 -0600

    Updated Header.jsx style

--- src/Header.jsx
+++ src/Header.jsx
@@ -1,20 +1,35 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import './Header.css';
+import Cart from './assets/Cart.png';
+import defaultProfile from './assets/defaultProfile.png';
+import Logo from './assets/Logo.png';
 
 function Header() {
   return (
     <header className="header">
       <div className="logo">
-        <Link to='/' style={{ textDecoration: 'none' }}>Logo</Link>
+        <Link to='/' >
+        <img src={Logo} alt="Logo" className="header-logo-image" />
+        </Link>
       </div>
-      <input type="text" className="search" placeholder="Search for Products" />
+      <input type="text" className="search" placeholder="Search for Products ..." />
       <div className="profile-cart">
+
+
         <div className="profile">
-          <Link to='/profile' style={{ textDecoration: 'none' }}>Profile</Link>
+          <Link to='/login' style={{ textDecoration: 'none' }}> Sign In</Link>
+        </div>
+
+        <div className="profile">
+          <Link to='/profile'>
+          <img src={defaultProfile} alt="Default" className="profile-logo-image" />
+          </Link>
         </div>
         <div className="cart">
-          <Link to='/cart' style={{ textDecoration: 'none' }}>Cart</Link>
+          <Link to='/cart'>
+          <img src={Cart} alt="Cart" className="header-cart-image" />
+          </Link>
         </div>
       </div>
     </header>
