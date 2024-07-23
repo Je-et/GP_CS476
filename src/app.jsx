1c42c68b5f6162114cbaca2d5160d426edfb9bf4
kkevin-r <kevinrathod2767@gmail.com>
2024-07-22 21:34:36 -0600

    Added employee dasboard button

--- src/app.jsx
+++ src/app.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route, useLocation, Link } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route, useLocation, Link, useNavigate } from 'react-router-dom';
 import './app.css';
 import Header from './Header';
 import ItemList from './Itemlist';
@@ -8,7 +8,9 @@ import Cart from './Cart';
 import Checkout from './Checkout';
 import Login from './Login';
 import Signup from './Signup';
-import MealPlanning from './MealPlanning'; // Correctly import MealPlanning
+import MealPlanning from './MealPlanning';
+import AppEmp from './EmpDash/AppEmp.jsx';
+import OrderHistory from "./EmpDash/OrderHistory"
 
 function App() {
   return (
@@ -23,13 +25,19 @@ function App() {
 
 function ConditionalHeader() {
   const location = useLocation();
-  const showHeader = location.pathname !== '/login' && location.pathname !== '/signup';
+  const showHeader = location.pathname !== '/login' && location.pathname !== '/signup' && location.pathname !== "/empdashboard" && location.pathname !== "/orderhistory";
   return showHeader ? <Header /> : null;
 }
 
 function MainContent() {
   const location = useLocation();
-  const showContent = location.pathname !== '/profile' && location.pathname !== '/cart' && location.pathname !== '/checkout' && location.pathname !== '/login' && location.pathname !== '/signup' && location.pathname !== '/mealplanning'; // Add MealPlanning route
+  const showContent = location.pathname !== '/profile' && location.pathname !== '/cart' && location.pathname !== '/checkout' && location.pathname !== '/login' && location.pathname !== '/signup' && location.pathname !== '/mealplanning' && location.pathname !== "/empdashboard" && location.pathname !== "/orderhistory"; // Add MealPlanning route
+
+  const navigate = useNavigate()
+
+  const gotToNewPage = () => {
+    navigate("/empdashboard");
+  }
 
   return (
     <>
@@ -43,6 +51,8 @@ function MainContent() {
           <button>Health</button>
           <button>Beauty</button>
           <button>Category</button>
+          <button onClick={() => gotToNewPage()}>Employee Dashboard </button>
+
         </div>
       )}
       <Routes>
@@ -52,6 +62,8 @@ function MainContent() {
         <Route path="/login" element={<Login />} />
         <Route path="/signup" element={<Signup />} />
         <Route path="/mealplanning" element={<MealPlanning />} />
+        <Route path="/empdashboard" element={<AppEmp />} />
+        <Route path="/orderhistory" element={<OrderHistory />} />
       </Routes>
       {showContent && <ItemList />}
     </>
