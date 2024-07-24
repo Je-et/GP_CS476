a791fab85e1720b6f933809b1520977a55c11493
JohnDelMando <jhnpnglnn@gmail.com>
2024-07-24 17:21:56 -0600

    updated profile.py

--- backend/profile.py
+++ backend/profile.py
@@ -1,6 +1,8 @@
 from flask_restx import Namespace, Resource, fields
 from flask_jwt_extended import jwt_required, get_jwt_identity
+from flask import request, jsonify
 from models import User
+from exts import db
 
 profile_ns = Namespace('profile', description='Profile related operations')
 
@@ -11,7 +13,6 @@ profile_model = profile_ns.model(
     }
 )
 
-# Returns the username and profile image of the user
 @profile_ns.route('/')
 class UserProfile(Resource):
 
@@ -20,7 +21,26 @@ class UserProfile(Resource):
     def get(self):
         current_user = get_jwt_identity()
         user = User.query.filter_by(username=current_user).first()
+
         if not user:
             profile_ns.abort(404, 'User not found')
 
         return user
+
+    @jwt_required()
+    @profile_ns.expect(profile_model)
+    def put(self):
+        current_user = get_jwt_identity()
+        user = User.query.filter_by(username=current_user).first()
+
+        if not user:
+            profile_ns.abort(404, 'User not found')
+
+        data = request.get_json()
+        profile_picture = data.get('profile_picture')
+
+        if profile_picture:
+            user.profile_picture = profile_picture
+
+        db.session.commit()
+        return jsonify({"message": "Profile updated successfully"})
