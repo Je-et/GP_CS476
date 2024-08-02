c5cecdf99ba3999d21bd6fbd036d248399544536
JohnDelMando <166464147+JohnDelMando@users.noreply.github.com>
2024-08-02 01:52:57 -0600

    added Logout button for OrderHistory.jsx

--- src/EmpDash/OrderHistoryHeader.jsx
+++ src/EmpDash/OrderHistoryHeader.jsx
@@ -11,20 +11,26 @@ const OrderHistoryHeader = () => {
         navigate(-1);
     }
 
+    const handleLogout = () => {
+        localStorage.removeItem('access_token');
+        localStorage.removeItem('refresh_token');
+        navigate('/login');
+      };
+
     return (
         <header className="header">
             <div className="logo">
-              
+
                     <img src={Logo} alt="Logo" className="header-logo" />
-             
+
             </div>
             <div className="vertical_line"></div>
             <div className="title"> GreenBasket</div>
             <div className="vertical_line"></div>
             <div className="profile-cart">
-                
+
                 <button onClick={() => goBack()} className="btn">Dashboard</button>
-                <div className="logout">Log Out</div>
+                <div className="logout" onClick={handleLogout}>Log Out</div>
             </div>
         </header>
     )
