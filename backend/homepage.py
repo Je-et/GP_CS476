94fe529323334e791372772c9ea350954b13f02d
kkevin-r <kevinrathod2767@gmail.com>
2024-08-02 19:03:13 -0600

    code clean up and addedcomments

--- backend/homepage.py
+++ backend/homepage.py
@@ -4,8 +4,12 @@ from werkzeug.security import generate_password_hash, check_password_hash
 from flask_jwt_extended import JWTManager, create_access_token, create_refresh_token, jwt_required
 from flask import Flask, request, jsonify
 
+
+# Establish a namespace for homepage related operations
 homepage_ns=Namespace('home', description='homepage realated operations')
 
+
+# Model defined for items with their fields
 item_model=homepage_ns.model(
     'Items',
     {
@@ -16,11 +20,3 @@ item_model=homepage_ns.model(
         "glutenFree": fields.Boolean(),
     }
 )
-'''
-@homepage_ns.route('/items')
-class Item(Resource):
-
-    @homepage_ns.expect(item_model)
-    def (self):
-        data = request.get_json()
-'''
