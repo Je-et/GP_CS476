205348dae0b81bca93b10a99762f2a45b30b8f64
kkevin-r <kevinrathod2767@gmail.com>
2024-08-02 19:02:39 -0600

    code clean up and addedcomments

--- backend/checkout.py
+++ backend/checkout.py
@@ -4,7 +4,11 @@ from models import User, CartItem, Order, OrderItem, CheckoutItem, Item
 from flask import jsonify, request
 from exts import db
 
+# Establish a namespacee for checkout related operationss
+
 checkout_ns = Namespace('checkout', description='Checkout related operations')
+
+# Model defined for processing the payment
 
 payment_model = checkout_ns.model(
     'Payment', {
@@ -14,17 +18,20 @@ payment_model = checkout_ns.model(
     }
 )
 
-# Processes the payment and creates an order
+# Defining a class(with route) that processes the payment and creates an order
 @checkout_ns.route('/payment')
 class ProcessPayment(Resource):
     @jwt_required()
     @checkout_ns.expect(payment_model)
     def post(self):
+
+        # fetching the payment data from the request
         data = request.get_json()
         ccNumber = data.get('ccNumber')
         expiry = data.get('expiry')
         ccv = data.get('ccv')
 
+        # validating the payment data if it is correct or not
         errors = {}
         if not ccNumber or len(ccNumber) != 16 or not ccNumber.isdigit():
             errors['ccNumber'] = 'Credit Card Number must be 16 digits'
@@ -33,9 +40,11 @@ class ProcessPayment(Resource):
         if not ccv or not (len(ccv) == 3 or len(ccv) == 4) or not ccv.isdigit():
             errors['ccv'] = 'Security Code must be 3 or 4 digits'
 
+        # will return errors if validation goes wrong
         if errors:
             return errors, 400
 
+        # get the current user 
         current_user = get_jwt_identity()
         user = User.query.filter_by(username=current_user).first()
         if not user:
@@ -73,7 +82,7 @@ class ProcessPayment(Resource):
 
         db.session.commit()
 
-        # Clear the cart
+        # Clear the cart of the user
         CartItem.query.filter_by(user_id=user.id).delete()
         db.session.commit()
 
@@ -93,6 +102,8 @@ class CheckoutItems(Resource):
         if not user:
             return {'message': 'User not found'}, 404
 
+    
+        # query all the orders for the current user
         orders = Order.query.filter_by(user_id=user.id).all()
         result = []
         for order in orders:
@@ -102,6 +113,9 @@ class CheckoutItems(Resource):
                 'status': order.status,
                 'items': []
             }
+
+
+            # query all the items for the current order
             order_items = OrderItem.query.filter_by(order_id=order.id).all()
             for order_item in order_items:
                 item = Item.query.filter_by(id=order_item.item_id).first()
